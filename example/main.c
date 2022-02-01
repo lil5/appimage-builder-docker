6fe701cb5dd044c1e8e0dba281ebaddbb83204a7
Lucian I. Last <li@last.nl>
2022-02-01 15:33:49 +0100

    Add example

---
+++ example/main.c
@@ -0,0 +1,17 @@
+// Simple C program to display "Hello World"
+
+// Replace me with a binary that opens a window
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hello world
+    printf("Hello World");
+  
+    return 0;
+}
